624350774977ea2db7cf360dba7a89545f90d318
Emilly <152908409+MillyGabrielly02@users.noreply.github.com>
2024-05-14 00:49:15 -0300

    javascript.js

--- javascript.js
+++ javascript.js
@@ -20,7 +20,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 From: email,
                 Subject: `E-mail enviado por ${nome}`,
                 Body: `Nome: ${nome}\nCelular: ${celular}\nE-mail: ${email}\nMensagem: ${mensagem}`
-            }).then(function(response) {
+
+            })
+            .then(function(response) {
                 // Se o e-mail for enviado com sucesso, exibe uma mensagem
                 alert('E-mail enviado com sucesso!');
                 // Limpa os campos do formulário
@@ -28,20 +30,25 @@ document.addEventListener('DOMContentLoaded', function() {
                 document.getElementById('celular').value = '';
                 document.getElementById('email').value = '';
                 document.getElementById('mensagem').value = '';
-            }).catch(function(error) {
+            })
+            .catch(function(error) {
                 // Se houver um erro no envio do e-mail, exibe uma mensagem de erro
                 alert('Erro ao enviar o e-mail. Por favor, tente novamente.');
+                console.error('Erro no envio do e-mail:', error);
             });
-        } else {
+            
+        } 
+        else {
             // Se algum campo estiver em branco, exibe um alerta
             alert('Por favor, preencha todos os campos do formulário.');
         }
     });
   });
 
+
     function openWhatsApp() {
         // Número de telefone para o qual você deseja enviar mensagens
-        var phoneNumber = '31994512892'
+        var phoneNumber = '+55(31)994512892'
 
         // Texto da mensagem que você deseja enviar
         var message = 'Olá! Gostaria de obter mais informações.';
@@ -52,3 +59,49 @@ document.addEventListener('DOMContentLoaded', function() {
         // Abrir a janela do WhatsApp
         window.open(url);
     }  
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        // Adiciona um evento de clique ao botão de envio
+        document.getElementById('emailForm').addEventListener('submit', function(e) {
+            e.preventDefault(); // Previne o comportamento padrão do formulário
+      
+            // Obtém os valores dos campos do formulário
+            const nome = document.getElementById('nome').value;
+            const email = document.getElementById('email').value;
+            const mensagem = document.getElementById('mensagem').value;
+      
+            // Verifica se todos os campos foram preenchidos
+            if (nome && celular && email && mensagem) {
+                // Envia o e-mail usando a biblioteca smtp.js
+                Email.send({
+                    Host: "smtp.gmail.com",
+                    Username: "arqdesingdeinteriores", // Coloque seu e-mail
+                    Password: "sua_senha", // Coloque sua senha
+                    To: "destinatario@example.com", // E-mail do destinatário
+                    From: email,
+                    Subject: `E-mail enviado por ${nome}`,
+                    Body: `Nome: ${nome}\nE-mail: ${email}\nMensagem: ${mensagem}`
+    
+                })
+                .then(function(response) {
+                    // Se o e-mail for enviado com sucesso, exibe uma mensagem
+                    alert('E-mail enviado com sucesso!');
+                    // Limpa os campos do formulário
+                    document.getElementById('nome').value = '';
+                    document.getElementById('email').value = '';
+                    document.getElementById('mensagem').value = '';
+                })
+                .catch(function(error) {
+                    // Se houver um erro no envio do e-mail, exibe uma mensagem de erro
+                    alert('Erro ao enviar o e-mail. Por favor, tente novamente.');
+                    console.error('Erro no envio do e-mail:', error);
+                });
+                
+            } 
+            else {
+                // Se algum campo estiver em branco, exibe um alerta
+                alert('Por favor, preencha todos os campos do formulário.');
+            }
+        });
+      });
